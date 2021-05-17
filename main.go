4a09eadbac1353088b78d9fe289b7ec0e96372a8
unknown <williamcaetano14@gmail.com>
2021-05-17 02:39:57 -0300

    :sparkles: Finished first part

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"github.com/WilliamCSA04/go-first-api/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
-  app := fiber.New()
+func setupRoutes(app *fiber.App) {
   app.Get("/", func(c *fiber.Ctx) error {
     return c.Status(fiber.StatusOK).JSON(fiber.Map{
         "success":  true,
         "message": "You are at the endpoint 😉",
     })
   })
+  
+  api := app.Group("/api")
+
+  api.Get("", func(c *fiber.Ctx) error {
+      return c.Status(fiber.StatusOK).JSON(fiber.Map{
+          "success": true,
+          "message": "You are at the api endpoint 😉",
+      })
+  })
+
+  routes.TodoRoute(api.Group("/todos"))
+}
+
+func main() {
+  app := fiber.New()
+
+  setupRoutes(app)
+
   err := app.Listen(":8000")
 
   if err != nil {
--- routes/todo.go
+++ routes/todo.go
@@ -7,5 +7,9 @@ import (
 
 func TodoRoute(route fiber.Router) {
   route.Get("", controllers.GetTodos)
+  route.Post("", controllers.CreateTodo)
+  route.Put("/:id", controllers.UpdateTodo)
+  route.Delete("/:id", controllers.DeleteTodo)
+  route.Get("/:id", controllers.GetTodo)
 }
 
