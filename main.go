0800678164074af54d714bd9aba0c1353c7da1f9
unknown <williamcaetano14@gmail.com>
2021-05-17 02:22:16 -0300

    :sparkles: Added first route

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+  app := fiber.New()
+  app.Get("/", func(c *fiber.Ctx) error {
+    return c.Status(fiber.StatusOK).JSON(fiber.Map{
+        "success":  true,
+        "message": "You are at the endpoint 😉",
+    })
+  })
+  err := app.Listen(":8000")
+
+  if err != nil {
+      panic(err)
+  }
+
+}
